15c04668ae18422f4e7dca6bfe0dd5c51f0556bf
Matthias Grill <matthias.grill@student.tugraz.at>
2025-04-22 20:00:52 +0200

    add image of me

--- src/lib/Landing.svelte
+++ src/lib/Landing.svelte
@@ -1,10 +1,32 @@
 <div
-    class="w-screen h-screen px-8 sm:pl-16 md:pl-32 flex justify-left text-center justify-left text-left items-center bg-regal-white text-regal-black font-heading text-6xl sm:text-7xl md:text-9xl font-bold"
+    class="w-screen h-screen flex items-center bg-regal-white text-regal-black font-heading text-6xl sm:text-7xl md:text-9xl font-bold p-4 sm:p-8 md:p-16"
 >
-    <div>
-        <h1 class="pb-4 sm:pb-8">Allrounder.</h1>
-        <h1 class="pb-4 sm:pb-8">Developer.</h1>
-        <h1 class="pb-4 sm:pb-8">Student.</h1>
-        <h1><span class="underline text-accent-green">Matthias</span>.</h1>
+    <div
+        class="w-full h-full grid grid-cols-1 md:grid-cols-2 gap-8 sm:gap-16 items-center text-regal-black"
+    >
+        <div class="text-center text-left mb-8 mt-16 sm:mb-16 sm:mt-32">
+            <h1 class="pb-4 sm:pb-8">Allrounder.</h1>
+            <h1 class="pb-4 sm:pb-8">Developer.</h1>
+            <h1 class="pb-4 sm:pb-8">Student.</h1>
+            <h1><span class="underline text-accent-green">Matthias</span>.</h1>
+        </div>
+        <div
+            class="relative w-full sm:w-md aspect-square p-4 m-auto flex flex-col justify-between items-center"
+        >
+            <span
+                class="absolute top-0 left-0 w-8 h-8 sm:w-16 sm:h-16 border-t-2 border-l-2 border-regal-black"
+            ></span>
+            <span
+                class="absolute top-0 right-0 w-8 h-8 sm:w-16 sm:h-16 border-t-2 border-r-2 border-regal-black"
+            ></span>
+            <span
+                class="absolute bottom-0 left-0 w-8 h-8 sm:w-16 sm:h-16 border-b-2 border-l-2 border-regal-black"
+            ></span>
+            <span
+                class="absolute bottom-0 right-0 w-8 h-8 sm:w-16 sm:h-16 border-b-2 border-r-2 border-regal-black"
+            ></span>
+
+            <img src="/me.png" alt="Matthias Grill" class="sm:w-md" />
+        </div>
     </div>
 </div>
